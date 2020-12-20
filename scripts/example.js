49eb95eabe3d5f4b12919ac95f64402462523686
rosieett <rosie.ett@gmail.com>
2020-12-20 12:35:46 -0500

    more coffee orders

--- scripts/example.js
+++ scripts/example.js
@@ -10,22 +10,6 @@ module.exports = (robot) => {
     return res.send('Great! What would you like to order?');
   })
 
-
-  // robot.hear(/Can I order a (.*)/i, function(msg) {
-  //   var order;
-  //     if (order == "coffee"){
-  //         return msg.send("Sure! What size?");
-  //     } else if (order == "iced coffee") {
-  //         return msg.send("Sure! What size?");
-  //     } else if (order == "cappuchino") {
-  //         return msg.send("What kind of milk?");
-  //     } else if (order == "latte") {
-  //         return msg.send("What kind of milk?");
-  //     } else {
-  //       return msg.reply("I\'m sorry, you can only order a coffee, latte, iced coffee, or cappuchino at this time.");
-  //     }
-  //   })
-
     robot.respond(/Can I order a (.*)/, function(msg) {
       var order;
       order = msg.match[1];
@@ -46,9 +30,9 @@ module.exports = (robot) => {
         default:
           return msg.reply("I\'m sorry, you can only order a coffee, latte, iced coffee, or cappuchino at this time.");
       }
-    });
+    })
 
-    robot.respond(/large/, function(res) {
+    robot.respond(/large (.*)/, function(res) {
       return msg.send(`Great! I\'ll have your large ${order} ready shortly`);
     })
 
