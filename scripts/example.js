d80f87261cab4daad224d484090643f94b0ba688
rosieett <rosie.ett@gmail.com>
2020-12-20 13:00:54 -0500

    more coffee orders

--- scripts/example.js
+++ scripts/example.js
@@ -10,96 +10,41 @@ module.exports = (robot) => {
     return res.send('Great! What would you like to order?');
   })
 
-    // robot.respond(/Can I order a (.*)/, function(msg) {
-    //   var order;
-    //   order = msg.match[1];
-    //   console.log(order);
-    //   switch (order) {
-    //     case "coffee":
-    //       return msg.reply("Sure! What size?");
-    //       break;
-    //     case "iced coffee":
-    //       return msg.reply("Sure! What size?");
-    //       break;
-    //     case "latte":
-    //       return msg.reply("What kind of milk?");
-    //       break;
-    //     case "cappuchino":
-    //       return msg.reply("What kind of milk?");
-    //       break;
-    //     default:
-    //       return msg.reply("I\'m sorry, you can only order a coffee, latte, iced coffee, or cappuchino at this time.");
-    //   }
-    // })
-
-    robot.respond(/Can I order a (.*)/, function(msg) {
-      var order;
-      order = msg.match[1];
-      console.log(order);
-      if (order == "coffee") {
-          return msg.reply("Sure! What size?");
-        } else if (order == "iced coffee") {
-          return msg.reply("Sure! What size?");
-        } else if (order == "latte") {
-          return msg.reply("What kind of milk?");
-        } else if (order == "cappuchino") {
-          return msg.reply("What kind of milk?");
-        }
-    })
-
-    robot.respond(/I'd like a (.*)/i, function(msg) {
-      var size;
-      size = msg.match[2];
-      console.log(size);
-      if (size == "large"){
-        return msg.send("Great! I\'ll have your large" + order + " ready shortly");
-      } else if (size == "medium") {
-        return msg.reply("Great! I\'ll have your medium" + order + " ready shortly");
-      } else if (size == "small") {
-        return msg.reply("Great! I\'ll have your small" + order + " ready shortly");
+  robot.respond(/Can I order a (.*)/, function(msg) {
+    var order;
+    order = msg.match[1];
+    console.log(order);
+    if (order == "coffee") {
+        return msg.reply("Sure! What size?");
+      } else if (order == "iced coffee") {
+        return msg.reply("Sure! What size?");
+      } else if (order == "latte") {
+        return msg.reply("What kind of milk?");
+      } else if (order == "cappuchino") {
+        return msg.reply("What kind of milk?");
       }
-    })
+  })
 
-    // robot.respond(/large/, function(res) {
-    //   return msg.reply("Great! I\'ll have your large" + order + " ready shortly");
-    // })
+  robot.respond(/I'd like a (.*)/i, function(msg) {
+    var size;
+    size = msg.match[2];
+    console.log(size);
+    if (size == "large"){
+      return msg.send("Great! I\'ll have your large" + order + " ready shortly");
+    } else if (size == "medium") {
+      return msg.reply("Great! I\'ll have your medium" + order + " ready shortly");
+    } else if (size == "small") {
+      return msg.reply("Great! I\'ll have your small" + order + " ready shortly");
+    }
+  })
 
-  // robot.hear(/milk (.*)/, function(msg) {
-  //   var milk;
-  //   milk = msg.match[1];
-  //   console.log(milk);
-  //   switch (milk) {
-  //     case "whole milk":
-  //       return msg.reply("whole milk, got it!");
-  //       break;
-  //     case "oat milk":
-  //       return msg.reply("oat milk, got it!");
-  //       break;
-  //     case "almond milk":
-  //       return msg.reply("almond milk, got it!");
-  //       break;
-  //     case "skim milk":
-  //         return msg.reply("skim milk, got it!");
-  //         break;
-  //     default:
-  //       return msg.reply("We have whole, skim, almond, or oat milk.");
-  //   }
-  // })
-  //
-  // robot.hear(/milk/, function(res) {
-  //   var milk;
-  //   milk = msg.match[1];
-  //   if (order == "whole milk") {
-  //     return msg.send("Great I\'ll have your " + withMilk + "with " + milk);
-  //   }
-  // })
 
-  // robot.hear(/whole milk/, function(res) {
-  //   return res.send("Great I\'ll have your " + withMilk + "with ");
-  // })
+  const drinks = ['coffee', 'iced coffee', 'latte', 'cappuchino']
+  const sizes = ['large', 'medium', 'small']
+  const milks = ['almond milk', 'oat milk', 'whole milk', 'skim milk']
 
   robot.hear(/Coffee Order Help/, function(res) {
-    return res.send('You should go to somewhere else');
+    res.send(res.random(size + milk + drinks));
   })
 
 
