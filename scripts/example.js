4c9b4f86fd980df949620461b3ac36210a87e8de
rosieett <rosie.ett@gmail.com>
2020-12-20 17:57:58 -0500

    more coffee orders

--- scripts/example.js
+++ scripts/example.js
@@ -4,62 +4,67 @@
 
 module.exports = (robot) => {
 
-  //Beginning prompt to ordering coffee
-  robot.hear(/I'm tired/, function(res) {
-    return res.send('You know what might help? Coffee!');
-  })
+//Beginning prompt to ordering coffee
+robot.hear(/I'm tired/, function(res) {
+  return res.send('You know what might help? Coffee!');
+})
 
-  //Telling the coffeebot your order
-  robot.respond(/can I order a (.*)/, function(msg) {
-    var order;
-    order = msg.match[1];
-    console.log(order);
-    if (order == "coffee") {
-        return msg.reply(`Sure! A ${order} is a great idea!`);
-      } else if (order == "iced coffee") {
-        return msg.reply(`Are you sure? It\'s a little cold for an ${order} but no judgement`);
-      } else if (order == "latte") {
-        return msg.reply(`Yummy and warming. A ${order} is a great idea!`);
-      } else if (order == "tea") {
-        return msg.reply(`Great choice. ${order} is my afternoon staple.`);
-      } else {
-        return msg.reply(`Sorry, we only have coffee, iced coffee, tea, and lattes at this time`);
-      }
-  })
+//Telling the coffeebot your order
+robot.respond(/can I order a (.*)/, function(msg) {
+  var order;
+  order = msg.match[1];
+  console.log(order);
+  if (order == "coffee") {
+    return msg.reply(`Sure! A ${order} is a great idea!`);
+  } else if (order == "iced coffee") {
+    return msg.reply(`Are you sure? It\'s a little cold for an ${order} but no judgement`);
+  } else if (order == "latte") {
+    return msg.reply(`Yummy and warming. A ${order} is a great idea!`);
+  } else if (order == "tea") {
+    return msg.reply(`Great choice. ${order} is my afternoon staple.`);
+  } else {
+    return msg.reply(`Sorry, we only have coffee, iced coffee, tea, and lattes at this time`);
+  }
+})
 
-  // Responding to the coffeebot's follow up questions in step 1
-  robot.respond(/I'd like something (.*)/i, function(msg) {
-    var feeling;
-    feeling = msg.match[1];
-    console.log(feeling);
-    if (feeling == "cold"){
-      return msg.send("Great! I\'ll have your iced coffee ready shortly");
-    } else if (feeling == "milk based") {
-      return msg.reply("Sounds like a latte is what you need");
-    } else if (feeling == "floral") {
-      return msg.reply("I'd recommend a hibiscus tea");
-    } else if (feeling == "hot") {
-      return msg.reply("Coffee. Definitely coffee.");
-    } else {
-      return msg.reply(`Feeling ${feeling}? When in doubt, drink seltzer.`)
-    }
-  })
+// Coffeebot recommendations depending on what you're feeling
+robot.respond(/I'd like something (.*)/i, function(msg) {
+  var feeling;
+  feeling = msg.match[1];
+  console.log(feeling);
+  if (feeling == "cold") {
+    return msg.send("You're in need of an iced coffee, for sure.");
+  } else if (feeling == "milk based") {
+    return msg.reply("Sounds like a latte is what you need");
+  } else if (feeling == "floral") {
+    return msg.reply("I'd recommend a hibiscus tea");
+  } else if (feeling == "hot") {
+    return msg.reply("Coffee. Definitely coffee.");
+  } else {
+    return msg.reply(`Feeling ${feeling}? When in doubt, drink seltzer.`)
+  }
+})
 
 
-  // Random task: having the bot randomly create a coffee order from the arrays below
-  const drinks = ['coffee', 'iced coffee', 'latte', 'cappuchino']
-  const sizes = ['large', 'medium', 'small']
-  const milks = ['almond milk', 'oat milk', 'whole milk', 'skim milk']
+// Random task: having the bot randomly create a coffee order from the arrays below
+const drinks = ['coffee', 'iced coffee', 'latte', 'cappuchino']
+const sizes = ['large', 'medium', 'small']
+const milks = ['almond milk', 'oat milk', 'whole milk', 'skim milk']
 
-  robot.hear(/Order something for me/, (res) => {
-      res.send('You should get a ' + res.random(sizes) + ' ' + res.random(milks) +' ' + res.random(drinks))
-    })
+robot.hear(/Order something for me/, (res) => {
+  res.send('You should get a ' + res.random(sizes) + ' ' + res.random(milks) + ' ' + res.random(drinks))
+})
 
-    //Responding to whoever wants coffee
-    // robot.hear /tired/i, (res) =>
-    //   room = "the-official-hubot-testing-org-for-ga-jsr-121"
-    //   robot.messageRoom room, "If you're tired, why not order some coffee!"
-    // }
+// Responding to the room if someone is tired
+robot.hear(/I'm tired/i, function(res) =>
+  room = "the-official-hubot-testing-org-for-ga-jsr-121"
+  robot.messageRoom room, "If you're tired, why not order some coffee!"
+)
 
+//Responding to the tired person specifically
+robot.hear(/I seriously need coffee/i, function(res) =>
+  room = "the-official-hubot-testing-org-for-ga-jsr-121", "coffeebot-ordering"
+  robot.messageRoom res.message.user.id, "I gotchu"
+)
 
 }
