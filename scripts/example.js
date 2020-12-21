162284f4e7942d1bebd5536d6eeb37834b9ae114
rosieett <rosie.ett@gmail.com>
2020-12-20 19:52:58 -0500

    dm fix

--- scripts/example.js
+++ scripts/example.js
@@ -63,8 +63,8 @@ robot.hear(/I need some caffeine/i, (res) {
 })
 
 // Responding to person who needs coffee
-  robot.respond(/Help I need some coffee/i, (res) => {
-    return robot.send(robot.messageRoom(res.`@rosie_ettenheim`, `I can help with that!`));
-  })
+  // robot.respond(/Help I need some coffee/i, (res) => {
+  //   return robot.send(robot.messageRoom(res.`@rosie_ettenheim`, `I can help with that!`));
+  // })
 
 };
